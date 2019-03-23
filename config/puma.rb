9b9dead188edf0af96492248d1a04c5c8aa946d0
Tan Chin Leong <cltan8@gmail.com>
2019-03-23 12:47:37 +0800

    Remove procfile. Change Procfile

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 app do |env|
    puts env
-   body = 'Hello, World from Puma!'
-   [200, { 'Content-Type' => 'text/plain', 'Content-Length' => body.length.to_s }, [body]]
+   body = "Hello, World from Puma!<br><br>#{env}"
+   [200, { 'Content-Type' => 'text/html', 'Content-Length' => body.length.to_s }, [body]]
 end
 
